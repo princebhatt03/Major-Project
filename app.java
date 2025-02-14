3df1962896b92d06ca9ae242d27de41cfad8dcb1
Prince Bhatt <princebhatt316@gmail.com>
2025-02-14 22:11:37 +0530

    Some More Dependencies added

--- app.java
+++ app.java
@@ -2,5 +2,7 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Major Project Source Code");
         System.out.println("This is the main class");
+        // Just Commit any changes
+        // Provide Commit Message: "Initialize project with basic folder structure"
     }
 }
