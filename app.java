aead33dd09c71ee3d5b2abca06c21942d25078c5
Prince Bhatt <princebhatt316@gmail.com>
2025-02-14 22:01:40 +0530

    Main Class and Variables added

--- app.java
+++ app.java
@@ -1,5 +1,6 @@
 public class app {
     public static void main(String[] args) {
         System.out.println("Major Project Source Code");
+        System.out.println("This is the main class");
     }
 }
