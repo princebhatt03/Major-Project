7b380f9c2a025202c7822a1a7b508fd979a3e2fd
Prince Bhatt <princebhatt316@gmail.com>
2025-02-17 17:27:37 +0530

    Branch Added

--- app.java
+++ app.java
@@ -2,7 +2,6 @@ public class app {
     public static void main(String[] args) {
         System.out.println("Major Project Source Code");
         System.out.println("This is the main class");
-        System.out.println("Demo Commit");
         // Just Commit any changes
         // Provide Commit Message: "Initialize project with basic folder structure"
     }
