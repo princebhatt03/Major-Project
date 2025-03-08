d197666a6d58205db5226bc14db051686259cf6c
Prince Bhatt <princebhatt316@gmail.com>
2025-03-08 11:22:44 +0530

    Testing Locally with Updated Functions

--- app.java
+++ app.java
@@ -1,9 +1,9 @@
 public class app {
     public static void main(String[] args) {
         System.out.println("Major Project Source Code");
-        // Just Commit any changes
-        // Provide Commit Message: "Software Features added, New Branch Added, Bugs
-        // fixed"
+        // ** Just Commit any changes **
+        // ** Provide Commit Message: "Software Features added, New Branch Added, Bugs
+        // fixed, Testing Ardiuno Connectivity Locally" **
         // Commands for Cloning the Project:
         // 1. git clone <link of Repo> : (cloning Repo)
         // 2. cd <then Press tab, and enter> : (going to project Directory)
