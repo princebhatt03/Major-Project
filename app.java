de739d28624a3bfcf54e8c8cdbff5df4b3d05f3f
Prince Bhatt <princebhatt316@gmail.com>
2025-03-08 11:20:02 +0530

    Bug Fixed, in the code, Testing code Locally

--- app.java
+++ app.java
@@ -1,8 +1,20 @@
 public class app {
     public static void main(String[] args) {
         System.out.println("Major Project Source Code");
-        System.out.println("This is the main class");
         // Just Commit any changes
-        // Provide Commit Message: "Software Features added, New Branch Added, Bugs fixed"
+        // Provide Commit Message: "Software Features added, New Branch Added, Bugs
+        // fixed"
+        // Commands for Cloning the Project:
+        // 1. git clone <link of Repo> : (cloning Repo)
+        // 2. cd <then Press tab, and enter> : (going to project Directory)
+        // 3. git branch -a : (Checking Branches)
+        // 4. git checkout -b Second-Branch : (Creating new Branch with Name :
+        // Second-Branch, you can create your own name)
+        // 5. git checkout Second-Branch : (switching to Second-Branch)
+        // ** Now Modified the Code to any files **
+        // 6. git add .
+        // 7. git commit -m "Commit Message"
+        // 8. git push origin Second-Branch
+        // ** After that I will Accept your changes **
     }
 }
